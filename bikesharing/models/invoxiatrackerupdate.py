30279e273657f95523d34d347cd5e9eeffc6ec3b
Fabian Monheim <fabian.monheim@leibniz-fli.de>
2021-01-21 15:07:30 +0100

    changed attributes from InvoxiaTrackerUpdate to optional attributes

--- bikesharing/models/invoxiatrackerupdate.py
+++ bikesharing/models/invoxiatrackerupdate.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class InvoxiaTrackerUpdate(models.Model):
     serial      = models.CharField(max_length=50)
-    datetime    = models.DateTimeField()
+    datetime    = models.DateTimeField(default=None, null=True, blank=True)
     lat         = models.FloatField()
     lng         = models.FloatField()
-    precision   = models.FloatField()
-    energy_level= models.FloatField()
+    precision   = models.FloatField(default=None, null=True, blank=True)
+    energy_level= models.FloatField(default=None, null=True, blank=True)
     reported = models.DateTimeField(default=None, null=True, blank=True)
